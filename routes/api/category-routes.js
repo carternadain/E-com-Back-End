0d4b435887164d9a75a29749a092156e25731cdc
carternadain <cnadain@gmail.com>
2022-04-23 19:43:10 -0700

    second router.get

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
  Category.findAll(
 {
  include:{
-   module: Product,
+   model: Product,
    attributes: ['product_name']
   }
  }
@@ -22,12 +22,27 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
+  Category.findOne({
+    where:{
+      id:req.params.id
+    },
+
+     include: {
+       model: Product,
+       attributes: ['category_id']
+      }
+     }
+    )
+      .then(categoryData => res.json(categoryData))
+      .catch(err =>{
+        console.log(err);
+        res.status(500).json(err);
+      });
+    });
+
 
 router.post('/', (req, res) => {
-  // create a new category
+   
 });
 
 router.put('/:id', (req, res) => {
